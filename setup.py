8dd6c31adc45fd0a8e659dbab3920445ebc8d56f
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2023-06-10 05:17:41 +0900

    www

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 
 setuptools.setup(
     name="sample_command",
-    version="0.9",
+    version="0.10",
     packages=setuptools.find_packages(),
 )
---
+++ woxram/van.py
@@ -0,0 +1,3 @@
+import woxram
+
+woxram.SampleClass()
